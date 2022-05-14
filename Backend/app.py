0d0589e915438f8f37400f9d164c25a197823dba
solar <1552443053@qq.com>
2022-05-14 21:51:23 +0800

    add backend

--- Backend/app.py
+++ Backend/app.py
@@ -1,6 +1,7 @@
 import detection.expression as ex
 from flask import Flask
 from flask import request
+from flask import jsonify, make_response
 import numpy as np
 from PIL import Image
 import io, cv2
@@ -8,10 +9,15 @@ import io, cv2
 app = Flask(__name__)
 
 @app.route("/getExpression", methods=['POST'])
-def get_frame():
+def get_expression():
 
-    f = request.files['file']
-    img = cv2.imdecode(np.frombuffer(f.stream.read(), np.uint8), cv2.IMREAD_COLOR)
-    return ex.getExpression(img)
+    try:
+        f = request.files['file']
+        img = cv2.imdecode(np.frombuffer(f.stream.read(), np.uint8), cv2.IMREAD_COLOR)
+        res = ex.getExpression(img)
+    except:
+        return make_response(jsonify({'data': None, 'status': 0, 'error': 'Something is wrong'}), 0)
+    
+    return make_response(jsonify({'data': res, 'status': 200, 'error': None}), 200)
 
 app.run(host='0.0.0.0', port=81)
--- Backend/detection/expression.py
+++ Backend/detection/expression.py
@@ -2,8 +2,8 @@ import cv2
 
 def getExpression(img):
     recognizer = cv2.face.LBPHFaceRecognizer_create()
-    recognizer.read('server/detection/model/model.yml')
-    cascadePath = "server/detection/model/haarcascade_frontalface_default.xml"
+    recognizer.read('backend/detection/model/model.yml')
+    cascadePath = "backend/detection/model/haarcascade_frontalface_default.xml"
     faceCascade = cv2.CascadeClassifier(cascadePath)
 
     id = 0
